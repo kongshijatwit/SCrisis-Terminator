bc972151ff6aed666c34cf3474bf475eba8875ed
UnSpoken12 <71053495+kongshijatwit@users.noreply.github.com>
2023-03-10 00:22:14 -0500

    Included LayerMask in raycast
    - Added "Interactable" layer
    - Future interactable objects will need IRaycastable AND be set to Layer "Interactable"
    - Fixed DialogueBox sticking out if you make the window large enough

--- SCrisis Terminator/Assets/Scripts/Player/PlayerRaycast.cs
+++ SCrisis Terminator/Assets/Scripts/Player/PlayerRaycast.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class PlayerRaycast : MonoBehaviour
 {
+    [SerializeField] private Camera playerCamera;
     private readonly float rayDistance = 2f;
 
     void Update()
     {
-        Vector3 origin = gameObject.transform.position;
-        Vector3 direction = gameObject.transform.forward;
-        if (Physics.Raycast(origin, direction, out RaycastHit hit, rayDistance))
+        Vector3 origin = playerCamera.transform.position - new Vector3(0, 1f, 0);
+        Vector3 direction = playerCamera.transform.forward;
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, rayDistance, LayerMask.GetMask("Interactable")))
         {
             hit.transform.GetComponent<IRaycastable>().HandleRaycast(this);
         }
