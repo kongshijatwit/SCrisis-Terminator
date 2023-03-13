6328b2238b15c53d51ac5e7af2eec8713a29d38f
UnSpoken12 <71053495+kongshijatwit@users.noreply.github.com>
2023-03-12 21:57:32 -0400

    Updated fomatting
    - Changed button picture
    - Reformatted code

--- SCrisis Terminator/Assets/Scripts/UI/LevelChanger.cs
+++ SCrisis Terminator/Assets/Scripts/UI/LevelChanger.cs
@@ -1,38 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
 public class LevelChanger : MonoBehaviour
 {
-    public Button startButton;
-    public Animator animator;
-    // var takes string value and loads it into next level
+    [SerializeField] private Button startButton;
+    private Animator animator;
     private string levelToLoad;
 
-    //Start is called before the first frame update
     void Start()
     {
-        //Finds Start button and set listner to begin Fade Animations
-        startButton.onClick.AddListener( delegate {FadeToLevel("World"); });
+        animator = GetComponent<Animator>();
         
+        // Finds Start button and set listner to begin Fade Animations
+        startButton.onClick.AddListener(() => { FadeToLevel("World"); });
     }
 
-    // Update is called once per frame
-    void Update()
+    // Triggers Fade out animations
+    public void FadeToLevel(string world)
     {
-        
-    }
-
-    // Triggers Fadeout animations
-   public void FadeToLevel (string World) {
-        levelToLoad = World;
+        levelToLoad = world;
         animator.SetTrigger("Fade out");
     }
-    //loads next level after fade has ended
-    public void OnFadeComplete(){
+
+    // Loads next level after fade has ended
+    public void OnFadeComplete()
+    {
         SceneManager.LoadScene(levelToLoad);
     }
 }
