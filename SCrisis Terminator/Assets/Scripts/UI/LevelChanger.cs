940e0ca490932eba884e0798f8de4051085061d4
UnSpoken12 <71053495+kongshijatwit@users.noreply.github.com>
2023-03-13 13:53:37 -0400

    Revert "Updated fomatting"
    This reverts commit 6328b2238b15c53d51ac5e7af2eec8713a29d38f.

--- SCrisis Terminator/Assets/Scripts/UI/LevelChanger.cs
+++ SCrisis Terminator/Assets/Scripts/UI/LevelChanger.cs
@@ -1,31 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
 public class LevelChanger : MonoBehaviour
 {
-    [SerializeField] private Button startButton;
-    private Animator animator;
+    public Button startButton;
+    public Animator animator;
+    // var takes string value and loads it into next level
     private string levelToLoad;
 
+    //Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
+        //Finds Start button and set listner to begin Fade Animations
+        startButton.onClick.AddListener( delegate {FadeToLevel("World"); });
         
-        // Finds Start button and set listner to begin Fade Animations
-        startButton.onClick.AddListener(() => { FadeToLevel("World"); });
     }
 
-    // Triggers Fade out animations
-    public void FadeToLevel(string world)
+    // Update is called once per frame
+    void Update()
     {
-        levelToLoad = world;
+        
+    }
+
+    // Triggers Fadeout animations
+   public void FadeToLevel (string World) {
+        levelToLoad = World;
         animator.SetTrigger("Fade out");
     }
-
-    // Loads next level after fade has ended
-    public void OnFadeComplete()
-    {
+    //loads next level after fade has ended
+    public void OnFadeComplete(){
         SceneManager.LoadScene(levelToLoad);
     }
 }
